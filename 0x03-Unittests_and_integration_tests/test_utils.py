f47a135a7d185151f766997d0baf939ba64eb693
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-02 17:44:47 +0300

    updated the _map_exception method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for access_nested_map function."""
+
     @parameterized.expand(
         [
         ({"a": 1}, ("a",), 1),
@@ -14,18 +15,17 @@ class TestAccessNestedMap(unittest.TestCase):
         ]
     )
 
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test return value of access_nested_map ."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
     @parameterized.expand(
         [
         ({}, ("a",), "'a'"),
         ({"a": 1}, ("a" "b",), "'b'"),
         ]
     )
-
-
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test return value of access_nested_map ."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
+    
     def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
         """Test KeyError raised for invalid path."""
         with self.assertRaises(KeyError) as context:
