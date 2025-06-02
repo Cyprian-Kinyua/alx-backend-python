184bfbb1ed5971c5c7215e46b31ea097dad92db9
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-02 17:41:21 +0300

    Implemented a test for keyerror raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,10 +14,23 @@ class TestAccessNestedMap(unittest.TestCase):
         ]
     )
 
+    @parameterized.expand(
+        [
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a" "b",), "'b'"),
+        ]
+    )
+
 
     def test_access_nested_map(self, nested_map, path, expected):
         """Test return value of access_nested_map ."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+        """Test KeyError raised for invalid path."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_exception_message)
 
 
 
