73ba0287c353eddeaf766152c522488fb5280da5
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-06 17:58:49 +0300

    Testing get_json
    Mocking HTTP calls using unittest.mock.patch to return mock objects with json method that returns a parameterized payload passed to get_json. We verify that the get method is called exactly once per input with test_url as argumetn. We also test that the get_json is equal to test_payload

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
@@ -32,7 +33,25 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), expected_exception_message)
 
+class TestGetJson(unittest.TestCase):
+    """Test cases for get_json function."""
 
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": "true"}),
+            ("http://holberton.io", {"payload": "false"}),
+        ]
+    )
 
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test return value of get_json."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
 
+        result = get_json(test_url)
 
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
