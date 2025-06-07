a85cbcb57256b06505fd05e6f9316997096c5d91
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-07 11:36:17 +0300

    adhering to pycodestyle and adding sufficient documentation of classes, modules, and functions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,42 +10,34 @@ from utils import access_nested_map, get_json, memoize
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for access_nested_map function."""
 
-    @parameterized.expand(
-    [
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-    ]
-    )
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test return value of access_nested_map ."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand(
-    [
+    @parameterized.expand([
         ({}, ("a",), "'a'"),
         ({"a": 1}, ("a", "b",), "'b'"),
-    ]
-    )
+    ])
     def test_access_nested_map_exception(
         self, nested_map, path, expected_exception_message):
-        """Test KeyError raised for invalid path."""
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_exception_message)
+            """Test KeyError raised for invalid path."""
+            with self.assertRaises(KeyError) as context:
+                access_nested_map(nested_map, path)
+            self.assertEqual(str(context.exception), expected_exception_message)
 
 
 class TestGetJson(unittest.TestCase):
     """Test cases for get_json function."""
 
-    @parameterized.expand(
-        [
+    @parameterized.expand([
             ("http://example.com", {"payload": "true"}),
             ("http://holberton.io", {"payload": "false"}),
-        ]
-    )
-
-
+        ])
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test return value of get_json."""
@@ -75,15 +67,14 @@ class TestMemoize(unittest.TestCase):
                 """ Memoized property calling a_method"""
 
                 return self.a_method()
-     
+
         with patch.object(
             TestClass, 'a_method', return_value=42) as mock_method:
-            obj = TestClass()
+                obj = TestClass()
 
-            result1 = obj.a_property
-            result2 = obj.a_property
+                result1 = obj.a_property
+                result2 = obj.a_property
 
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
-     
+                self.assertEqual(result1, 42)
+                self.assertEqual(result2, 42)
+                mock_method.assert_called_once()
