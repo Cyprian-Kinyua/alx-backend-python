b1dcf58645416659b14806a3bfb87b553bfc95c3
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-07 11:27:08 +0300

    adhering to pycodestyle and adding sufficient documentation of classes, modules, and functions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,7 +27,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b",), "'b'"),
     ]
     )
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+    def test_access_nested_map_exception(
+        self, nested_map, path, expected_exception_message):
         """Test KeyError raised for invalid path."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
@@ -44,18 +45,17 @@ class TestGetJson(unittest.TestCase):
         ]
     )
 
+
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test return value of get_json."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
-
         result = get_json(test_url)
-
         mock_get.assert_called_once_with(test_url)
-
         self.assertEqual(result, test_payload)
+
 
 class TestMemoize(unittest.TestCase):
     """"Test that memoize decorator caches return value of a method."""
@@ -75,8 +75,9 @@ class TestMemoize(unittest.TestCase):
                 """ Memoized property calling a_method"""
 
                 return self.a_method()
-            
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+     
+        with patch.object(
+            TestClass, 'a_method', return_value=42) as mock_method:
             obj = TestClass()
 
             result1 = obj.a_property
@@ -84,6 +85,5 @@ class TestMemoize(unittest.TestCase):
 
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
-
             mock_method.assert_called_once()
-            
+     
