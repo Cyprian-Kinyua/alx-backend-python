02403d1f7c07648db22f8e102c4da37fb7c48b81
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-07 10:11:57 +0300

    Testinf memoize
    Implemented a test for memoize methodwith a mock method. Also test that when calling the method twice, the correct result is returned onlbut the method id called only once

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,3 +55,27 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """"Test that memoize decorator caches return value of a method."""
+    def test_memoize(self):
+        """" Test that memoized property calls method only once."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            
+            mock_method.assert_called_once()
+            
