9522a6a8e4750bbc332a1836ca806d1ab43925d2
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-08 09:53:19 +0300

    debugging utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -16,7 +16,7 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
     """Access a nested dictionary map using a path of keys."""
     for key in path:
         if not isinstance(nested_map, Mapping):
-            raise KeyError({key})
+            raise KeyError(key)
         nested_map = nested_map[key]
     return nested_map
 
@@ -33,6 +33,7 @@ def memoize(fn: Callable) -> Callable:
 
     @wraps(fn)
     def memoized(self):
+        """Memoized wrapsfunction."""
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
