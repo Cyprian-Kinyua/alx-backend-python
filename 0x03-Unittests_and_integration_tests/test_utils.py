adfc335be724f7a0075aa96593e275cca453aa9c
Cyprian Kings <kiamacyprian6@gmail.com>
2025-06-08 09:40:43 +0300

    Pycode style adherence and documenting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json, memoize
+from utils import *
 
 
 class TestAccessNestedMap(unittest.TestCase):
